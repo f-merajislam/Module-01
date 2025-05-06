260c8ad2068b792905cc47eaadd5b59b7c93dc28
Meraj <f.merajislam@gmail.com>
2025-05-06 10:27:13 +0600

    newproject

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
-$age = 25; // You can change this value to test different conditions
 
-if ($age >= 18) {
-    echo "You are an adult.";
-} else {
-    echo "You are a minor.";
-}
-else{
-    echo "You are a junior.";
-}
+$a = 30;
+$b = 30;
+
+//$c = ($a == $b) ? "Those are equal" : "Those are not equal";
+//echo $c."\n";
+
+//echo $c = ($a == $b) ? "Those are equal" : "Those are not equal";
+//echo "\n";
+//echo ($a != $b) ? "Those are equal" : "Those are not equal";
+
+
+
+
+
+
+
+
+?>
